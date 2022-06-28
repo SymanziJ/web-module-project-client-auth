b8d86fc466fdde5fb4d5c553bd249f08b41c702d
SymanziJ <97126299+SymanziJ@users.noreply.github.com>
2022-06-28 00:30:28 -0400

    routes set up

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+
+const Login = () => {
+  return (<h1>Login</h1>)
+}
+
+const FriendsList = () => {
+  return (<h1>Friends List</h1>)
+}
+
+const AddFriends = () => {
+  return (<h1>Add Friend</h1>)
+}
 
 function App() {
   return (
-    <div className="App">
-      <h2>Client Auth Project</h2>
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+          <Route exact path="/">
+            <Login/>
+          </Route>
+          <Route path="/login">
+            <Redirect to="/" />
+          </Route>
+          <Route path="/friends">
+            <FriendsList/>
+          </Route>
+          <Route path="/addfriends">
+            <AddFriends/>
+          </Route>  
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
